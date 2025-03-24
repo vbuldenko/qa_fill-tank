89b6d4f23ee84b0cdfa7ebde518c01724ccd507d
Vladyslav <vladbuldenko@gmail.com>
2025-03-24 11:42:51 +0200

    VB QA solution

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,11 +1,111 @@
 'use strict';
 
 describe('fillTank', () => {
-  // const { fillTank } = require('./fillTank');
+  const { fillTank } = require('./fillTank');
 
-  it('should ', () => {
-
+  it('should be defined', () => {
+    expect(fillTank).toBeDefined();
   });
 
-  // write tests here
+  it('should be an instance of function', () => {
+    expect(fillTank).toBeInstanceOf(Function);
+  });
+
+  it('should return undefined', () => {
+    const customer = {
+      money: 100,
+      vehicle: {
+        maxTankCapacity: 60,
+        fuelRemains: 10,
+      },
+    };
+
+    expect(fillTank(customer, 50, 50)).toBeUndefined();
+  });
+
+  it('should fill customer to max tack capasity without amount arg', () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 60,
+        fuelRemains: 10,
+      },
+    };
+
+    fillTank(customer, 50);
+
+    expect(customer.vehicle.fuelRemains).toBe(60);
+  });
+
+  it('should fill customer to max tack capasity if amount > capacity', () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 60,
+        fuelRemains: 10,
+      },
+    };
+
+    fillTank(customer, 50, 100);
+
+    expect(customer.vehicle.fuelRemains).toBe(60);
+  });
+
+  it('should fill only what can be paid', () => {
+    const customer = {
+      money: 100,
+      vehicle: {
+        maxTankCapacity: 60,
+        fuelRemains: 10,
+      },
+    };
+
+    fillTank(customer, 50);
+
+    expect(customer.vehicle.fuelRemains).toBe(12);
+  });
+
+  it('should round the poured amount to the tenth part', () => {
+    const customer = {
+      money: 1000,
+      vehicle: {
+        maxTankCapacity: 60,
+        fuelRemains: 0,
+      },
+    };
+
+    fillTank(customer, 50, 3.66);
+
+    expect(customer.vehicle.fuelRemains).toBe(3.6);
+  });
+
+  it('should not pour if poured amount < 2', () => {
+    const customer = {
+      money: 1000,
+      vehicle: {
+        maxTankCapacity: 60,
+        fuelRemains: 0,
+      },
+    };
+
+    fillTank(customer, 50, 1.7);
+
+    expect(customer.vehicle.fuelRemains).toBe(0);
+  });
+
+  it('should round the purchased fuel price to the hundredth part', () => {
+    const customer = {
+      money: 200,
+      vehicle: {
+        maxTankCapacity: 60,
+        fuelRemains: 0,
+      },
+    };
+
+    const price = 50.666;
+
+    fillTank(customer, price, 2);
+
+    expect(customer.money).toBe(200 - (2 * price).toFixed(2));
+  });
 });
